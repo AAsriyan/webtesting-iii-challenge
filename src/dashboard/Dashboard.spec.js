e20e9c0976859b1bbbb7148137be8c280029b02e
AAsriyan <aasriyan@protonmail.com>
2019-04-10 18:02:52 -0400

    Added testing for dashboard, display, and controls components

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,21 @@
+import React from "react";
+import { render, fireEvent, cleanup } from "react-testing-library";
+import renderer from "react-test-renderer";
+import "react-testing-library/cleanup-after-each";
+
+import Dashboard from "./Dashboard.js";
+
 // Test away
+
+afterEach(() => {
+  cleanup();
+  console.log(document.body.outerHTML);
+});
+
+describe("<Dashboard />", () => {
+  it.skip("matches snapshot", () => {
+    const tree = renderer.create(<Dashboard />);
+
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
